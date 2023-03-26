982175d338b9c77b2c6558c0be78b3698e94ee56
k33g <ph.charriere@gmail.com>
2023-03-26 00:32:16 +0000

    save

---
+++ build.image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o allexport; source .env; set +o allexport
+echo "🐋 ${IMAGE_NAME}-amd64:${IMAGE_TAG}"
+
+docker login -u ${DOCKER_USER} -p ${DOCKER_PWD}
+docker buildx build --platform linux/amd64 -t ${IMAGE_NAME} .
+
+docker images | grep ${IMAGE_NAME}
+
+# docker buildx ls
